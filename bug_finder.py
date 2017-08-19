4d063a0780aa5f6d86fd1831ba1742159872adf0
lxTHU <lixuanTHU@outlook.com>
2017-08-19 17:53:45 +0800

    Testing function

--- bug_finder.py
+++ bug_finder.py
@@ -1,11 +1,15 @@
-#%%
-#from __future__ import division
+# -*- coding: utf-8 -*-
+# bug finder
+"""Testing deltasigma toolbox's function, find bug, or verify function.
+"""
+
+# %%
+# from __future__ import division
 from deltasigma import synthesizeNTF
-#from deltasigma._synthesizeNTF1 import synthesizeNTF1
-#import deltasigma.synthesizeNTF as synthesizeNTF
+# import deltasigma as ds
+# from deltasigma._synthesizeNTF1 import synthesizeNTF1
+# import deltasigma.synthesizeNTF as synthesizeNTF
 
-order = 6
-OSR = 16
 
-H0 = synthesizeNTF(order,OSR,0,1.5,0.5)
+H0 = synthesizeNTF(order=6, osr=16, opt=0, H_inf=1.5, f0=0.5)
 print(H0)
