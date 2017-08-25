4febe390b3ca6012c1379315cd901e4208e25a5e
lxTHU <lixuanTHU@outlook.com>
2017-08-25 16:04:41 +0800

    add save, replace N to NS

--- bug_finder.py
+++ bug_finder.py
@@ -19,11 +19,11 @@ h0p5 = synthesizeNTF(order=ORDER, osr=OSR, opt=0, H_inf=1.5, f0=FC)
 print(h0p5)
 
 #%%
-N = 8192
-FB = int(np.ceil(N / (2. * OSR)))
+NS = 8192
+FB = int(np.ceil(NS / (2. * OSR)))
 # ftest = np.floor(2. / 3. * FB)  #for LPF_DSM
-ftest = N//4 - np.floor(1. / 8. * FB) #for HPF_DSM
-input_cos = 0.5 * np.sin(2 * np.pi * ftest * np.arange(N) / N)
+ftest = NS//4 - np.floor(1. / 8. * FB) #for HPF_DSM
+input_cos = 0.5 * np.sin(2 * np.pi * ftest * np.arange(NS) / NS)
 output_DSM, xn, xmax, y = simulateDSM(input_cos, h0p5)
 t = np.arange(301)
 
@@ -40,9 +40,9 @@ plt.show()
 # plt.hold(True)
 
 #%%
-f = np.linspace(0, 0.5, N / 2. + 1)
-spec = np.fft.fft(output_DSM * ds_hann(N)) / (N / 4)
-plt.plot(f, dbv(spec[:int(N / 2. + 1)]), 'b', label='Simulation')
+f = np.linspace(0, 0.5, NS / 2. + 1)
+spec = np.fft.fft(output_DSM * ds_hann(NS)) / (NS / 4)
+plt.plot(f, dbv(spec[:int(NS / 2. + 1)]), 'b', label='Simulation')
 figureMagic([0, 0.5], 0.05, None, [-140, 0], 20,
             None, (16, 6), 'Output Spectrum')
 plt.xlabel('Normalized Frequency')
@@ -50,9 +50,12 @@ plt.ylabel('dBFS')
 snr = calculateSNR(spec[2:FB + 1], ftest - 2) #for LPF_DSM
 plt.text(0.05, -10, 'SNR = %4.1fdB @ OSR = %d' %
          (snr, OSR), verticalalignment='center')
-NBW = 1.5 / N
+NBW = 1.5 / NS
 Sqq = 4 * evalTF(h0p5, np.exp(2j * np.pi * f)) ** 2 / 3.
 plt.plot(f, dbp(Sqq * NBW), 'm', linewidth=2, label='Expected PSD')
 plt.text(0.49, -90, 'NBW = %4.1E x $f_s$' % NBW, horizontalalignment='right')
 plt.legend(loc=4)
 plt.show()
+
+#%% output data to files
+# np.savetxt('DacDsmOut.txt', output_DSM, fmt='%d')
